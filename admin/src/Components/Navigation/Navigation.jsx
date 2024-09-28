4c7272ffa431e3fb09e4b6ccf97e9e9d3efd6326
Rajibul2003 <rajibulmazhar@gmail.com>
2024-09-28 21:47:32 +0530

    navigation icon updated

--- admin/src/Components/Navigation/Navigation.jsx
+++ admin/src/Components/Navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-ant-path';
