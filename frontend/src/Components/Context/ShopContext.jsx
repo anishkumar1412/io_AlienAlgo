37ff74650a242ac3b8a604cc9c370ea35fa5583f
anishkumar1412 <106010120+anishkumar1412@users.noreply.github.com>
2024-11-06 00:30:27 +0530

    Update ShopContext.jsx

--- frontend/src/Components/Context/ShopContext.jsx
+++ frontend/src/Components/Context/ShopContext.jsx
@@ -22,13 +22,13 @@ const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
     useEffect(() => {
-        fetch('http://localhost:4000/allproducts')
+        fetch('io-alienalgo.onrender.com/allproducts')
             .then((response) => response.json())
             .then((data) => setAll_product(data))
 
         if (localStorage.getItem('auth-token')) {
             console.log("fuctino is running")
-            fetch('http://localhost:4000/getcart', {
+            fetch('io-alienalgo.onrender.com/getcart', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/form-data',
@@ -43,7 +43,7 @@ const ShopContextProvider = (props) => {
     const addToCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         if (localStorage.getItem('auth-token')) {
-            fetch('http://localhost:4000/addtocart', {
+            fetch('io-alienalgo.onrender.com/addtocart', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/form-data',
@@ -61,7 +61,7 @@ const ShopContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
         if (localStorage.getItem('auth-token')) {
-            fetch('http://localhost:4000/removefromcart', {
+            fetch('io-alienalgo.onrender.com/removefromcart', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/form-data',
